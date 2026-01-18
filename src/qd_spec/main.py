2ea45fe6c80579b68349fed75e06c359cb7f08c9
John Kline <jpkline43@gmail.com>
2026-01-18 16:31:48 -0600

    Make blend with term

--- src/qd_spec/main.py
+++ src/qd_spec/main.py
@@ -1,12 +1,30 @@
 import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
 
 from .fitter import fit_dg
 
+FG_COLOR = "#F2F2F2"
+BG_COLOR = "#0C0C0C"
+
 
 def main():
     matplotlib.use("module://matplotlib-sixel-backend")
-    # plt.style.use("dark_background")
+    plt.style.use("dark_background")
+    plt.rcParams.update(
+        {
+            "figure.facecolor": BG_COLOR,
+            "axes.facecolor": BG_COLOR,
+            "savefig.facecolor": BG_COLOR,
+            #
+            "text.color": FG_COLOR,
+            "axes.labelcolor": FG_COLOR,
+            "axes.titlecolor": FG_COLOR,
+            "xtick.color": FG_COLOR,
+            "ytick.color": FG_COLOR,
+            "axes.edgecolor": FG_COLOR,
+        }
+    )
 
     file = pd.read_csv(r"D:\qd_anal\Raw_Spectra\OldSetup\new_emis.csv", names=["nm", "Abs"])
     fit_dg(file["nm"], file["Abs"])
