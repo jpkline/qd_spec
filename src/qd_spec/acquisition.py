291be962c653da8aca95e6ee39ce5f5e03ecac52
John Kline <jpkline43@gmail.com>
2026-01-18 20:02:16 -0600

    Add license boilerplate

--- src/qd_spec/acquisition.py
+++ src/qd_spec/acquisition.py
@@ -1,4 +1,6 @@
 # Copyright 2024 StellarNet, Inc.
+# Copyright 2026 John Kline
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
